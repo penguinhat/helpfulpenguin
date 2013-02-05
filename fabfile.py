aca9f09df321613647f713e90aa2788ad80ece9e
William David Hunt <william.david.hunt@gmail.com>
2013-02-05 20:22:30 +0000

    Split deploy fabtask into pull and deploy tasks

--- fabfile.py
+++ fabfile.py
@@ -4,16 +4,21 @@ env.roledefs = {
     'web': ['151.236.220.208']
 }
 
+directory = '/srv/helpfulpenguin/django/'
+
 def test():
     local("./manage.py test")
 
+def pull(branch='master'):
+    with cd(directory):
+        run("git checkout %s" % branch)
+        run("git pull")
+
 def deploy():
-    directory = '/srv/helpfulpenguin/django/'
     with cd(directory):
         # Activate virtualenv
         python = "../venv/bin/python"
 
-        run("git pull")
         run("%s manage.py collectstatic --noinput" % python)
         run("%s manage.py migrate" % python)
         run("touch helpfulpenguin/wsgi.py")
