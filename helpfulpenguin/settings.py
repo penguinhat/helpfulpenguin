e4724a44ae684cee1afeadd220918d69b1f83c80
William David Hunt <william.david.hunt@gmail.com>
2013-02-05 19:13:48 +0000

    Moved DATABASES and STATIC_ROOT to local_settings

--- helpfulpenguin/settings.py
+++ helpfulpenguin/settings.py
@@ -1,5 +1,7 @@
 # Django settings for helpfulpenguin project.
 
+from local_settings import DATABASES, STATIC_ROOT
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -8,17 +10,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/whunt/Dev/helpfulpenguin/db.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -56,9 +47,6 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-
-#TODO: Move to local_settings.py
-STATIC_ROOT = '/home/whunt/Dev/helpfulpenguin/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
