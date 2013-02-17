5cf9bc95857b95dbe6728afd2c1c3641ccf20fe3
William David Hunt <william.david.hunt@gmail.com>
2013-02-17 18:37:16 +0000

    Allow crossdomain requests

--- helpfulpenguin/settings.py
+++ helpfulpenguin/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'redirects.middleware.django-crossdomainxhr-middleware.XsSharing'
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
