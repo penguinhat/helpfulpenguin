9e6bfec7e5515bfe3576bef8310fd28476dda17c
William David Hunt <william.david.hunt@gmail.com>
2013-02-17 18:57:27 +0000

    Add bookmarklet

--- helpfulpenguin/settings.py
+++ helpfulpenguin/settings.py
@@ -109,6 +109,7 @@ INSTALLED_APPS = (
 
     'frontend',
     'redirects',
+    'bookmarklet',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
